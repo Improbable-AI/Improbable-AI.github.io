93fce62d3cf53d7070474486aea82d88e1c2d2ad
younghyopark <younghyo@mit.edu>
2025-06-23 10:49:58 -0400

    init

--- src/components/joinArray.js
+++
@@ -1,8 +0,0 @@
-export default function joinArray(array, separator) {
-  return array.reduce((p, c, idx) => {
-    if (idx === 0)
-      return [c];
-    else
-      return [...p, separator, c];
-  }, []);
-}
